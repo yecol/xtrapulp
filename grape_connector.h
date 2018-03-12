9ec153bc81898e35579e7305f375173a73e53f67
Jingbo <xujb1987@gmail.com>
2018-03-12 23:00:24 +0800

    update.

---
+++ grape_connector.h
@@ -0,0 +1,33 @@
+#ifndef GRAPE_CONNECTOR_H_
+#define GRAPE_CONNECTOR_H_
+
+#include <getopt.h>
+#include <mpi.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "comms.h"
+#include "config.h"
+#include "dist_graph.h"
+#include "generate.h"
+#include "io_pp.h"
+#include "pulp_util.h"
+#include "util.h"
+#include "xtrapulp.h"
+
+namespace pulp {
+
+extern int procid, nprocs;
+extern int seed;
+extern bool verbose, debug, verify;
+void grape_connector(MPI_Comm comm_, int workernum, int workerID, int partnum,
+                     std::vector<VertexID> input_edges, int32_t *&final_parts,
+                     uint64_t &total_vnum, uint64_t file_size);
+}
+#endif  // GRAPE_CONNECTOR_H_
